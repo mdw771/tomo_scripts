d884f6956d1992204eb539b2999a3b8d8f55eefa
Ming <mingdu2015@u.northwestern.edu>
2017-10-19 14:50:51 -0500

    md

--- replace_white_fields_batch.py
+++ replace_white_fields_batch.py
@@ -5,27 +5,37 @@ import glob
 import os
 
 # ================================================
-folder = '.'
+folder_pattern = 'Cox*'
 source_file = '../flat_fields_tiltfixed.tiff'
 pattern = '*.h5'
 # ================================================
 
+temp = glob.glob(folder_pattern)
+folder_list = []
+for i in temp:
+    if os.path.isdir(i):
+        folder_list.append(i)
+
 root = os.getcwd()
-os.chdir(folder)
-filelist = glob.glob(pattern)
-os.chdir(root)
 
-flat_good = dxchange.read_tiff(source_file)
+for folder in folder_list:
+    os.chdir(folder)
+    filelist = glob.glob(pattern)
+    os.chdir(root)
 
-for fname in filelist:
-    print(fname)
-    fname = os.path.join(folder, fname)
-    f = h5py.File(fname)
-    dset = f['exchange/data_white']
-    if dset.shape[0] < flat_good.shape[0]:
-        dset[...] = flat_good[:dset.shape[0], :, :]
-    else:
-        for i in range(0, dset.shape[0], flat_good.shape[0]):
-            end = min([i+flat_good.shape[0], dset.shape[0]])
-            dset[i:end, :, :] = flat_good[:end-i, :, :]
-    f.close()
+    flat_good = dxchange.read_tiff(source_file)
+    if flat_good.ndim == 2:
+        flat_good = flat_good[np.newaxis, :, :]
+
+    for fname in filelist:
+        print(fname)
+        fname = os.path.join(folder, fname)
+        f = h5py.File(fname)
+        dset = f['exchange/data_white']
+        if dset.shape[0] < flat_good.shape[0]:
+            dset[...] = flat_good[:dset.shape[0], :, :]
+        else:
+            for i in range(0, dset.shape[0], flat_good.shape[0]):
+                end = min([i+flat_good.shape[0], dset.shape[0]])
+                dset[i:end, :, :] = flat_good[:end-i, :, :]
+        f.close()
