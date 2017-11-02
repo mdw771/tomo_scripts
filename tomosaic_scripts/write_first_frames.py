14917cce50bd9e681643a6ed7857758fed43e93f
Ming Du <mingdu2015@u.northwestern.edu>
2017-11-02 14:06:09 -0500

    m

--- tomosaic_scripts/write_first_frames.py
+++ tomosaic_scripts/write_first_frames.py
@@ -10,6 +10,6 @@ folder = 'data_raw_1x'
 
 for (y, x), f in np.ndenumerate(file_grid):
     print(y, x)
-    dat, flt, drk = read_data_adaptive(os.path.join(folder, f), proj=(0, 1), data_format='aps_13bm')
+    dat, flt, drk, _ = read_data_adaptive(os.path.join(folder, f), proj=(0, 1), data_format=data_format)
     dat = tomopy.normalize(dat, flt, drk)
     dxchange.write_tiff(dat, 'first_frames/y{}x{}'.format(y, x), dtype='float32', overwrite=True)
